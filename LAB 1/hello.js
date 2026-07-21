7bd048f0ecf729f6690512982daa1726eb22fb6e
abdul malik khan <am6690707@gmail.com>
2026-07-21 18:01:55 +0530

    add two number

--- LAB 1/hello.js
+++ LAB 1/hello.js
@@ -2,4 +2,14 @@ function sayHello(name){
     console.log(`Welcome to ${name}`);
 }
 
+const sayHi = (name) => {
+    console.log(`Welcome to ${name}`);
+}
+
+function sum(a,b){
+    return a+b;
+}
+
 sayHello('Abdul Malik Khan')
+sayHi("Abdul")
+console.log(`sum of 2 and 5 is = ${sum(2,5)}`);
