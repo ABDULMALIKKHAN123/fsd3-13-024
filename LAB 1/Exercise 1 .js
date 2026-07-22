a1c38a73fa2751f78694b52d2062677e0de0ef76
abdul malik khan <am6690707@gmail.com>
2026-07-22 11:52:32 +0530

    word to number vive versa

---
+++ LAB 1/Exercise 1 .js
@@ -0,0 +1,52 @@
+// write a fn to take number 0 to 9 and written in words
+const toWords = (digit)=> {
+    switch(digit){
+        case 1 :
+            return "One";
+            case 2 :
+                return "Two";
+                case 3 :
+                    return "Three";
+                    case 4:
+                    return "Four";
+                    case 5 :
+                        return "Five";
+                        case 6:
+                            return "Six";
+                            case 7 :
+                                return "seven";
+                                case 8:
+                                    return "Eight";
+                                    case 9:
+                                        return "Nine";
+                                            case 0:
+                                                return "Zero";
+                    
+    }
+}
+
+console.log(toWords(0));
+console.log(toWords(1));
+console.log(toWords(2));
+console.log(toWords(3));
+console.log (toWords(5));
+console.log (toWords(3));
+console.log (toWords(6));
+
+
+// create another fn that take a no and show into word fn
+
+const rollNum ="2503201000024";
+const digits = String(rollNum).split("");
+console.log(digits);
+
+let inWords = "";
+
+//syntax
+//array name.forEach((c) =>{} );
+
+digits.forEach((d) => {
+    inWords += " " + toWords(Number(d));
+    // console.log(toWords(Number(d)));
+});
+console.log(inWords);
---
+++ LAB 1/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+const rollNum ="2503201000024";
+// const digits = String(rollNum).split("");
+// console.log(digits);
