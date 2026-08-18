70a08e7d596240929f4b8f22a22d60ff9ff0efaf
abdul malik khan <am6690707@gmail.com>
2026-08-18 14:46:21 +0530

    add save cart and get cart

--- LAB 2/1 CRUD.js
+++ LAB 2/1 CRUD.js
@@ -1,5 +1,24 @@
 import readline from 'readline/promises'
+import {writeFile,readFile } from"fs/promises:
+
 import {stdin,stdout} from "process"
+import { from } from 'node:stream/iter'
+
+
+const FILE = "products.json"
+const savecart=(cart)=>{
+    await writeFile(FILE,JSON.stringify(cart,null,2));
+    
+};
+
+const getcart = async()=>{
+    const data = await readFile(FILE,"utf-8");
+    return JSON.parse(data);
+
+};
+
+
+
 
 
 
