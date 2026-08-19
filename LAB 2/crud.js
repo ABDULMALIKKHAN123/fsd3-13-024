02c416500929a5c23e01d54d272da83fd43a8a4b
abdul malik khan <am6690707@gmail.com>
2026-08-19 12:20:36 +0530

    agar koi product bik gaya to cart se remove ho jayega aur msg aayega

--- LAB 2/crud.js
+++ LAB 2/crud.js
@@ -102,8 +102,19 @@ const getCart = async () => {
 
 const addToCart = async (item) => {
   const products = await getCart();
-  products.push(item);
+  const productFound = products.find ((p)=>p.id==item.id);
+  if (productFound){
+    productFound.qty += item.qty;
+    console.log('product in cart quantity updated');
+  }
+  else{
+    product.push(item);
+    console.log("Product added successfully 👍");
+    
+  }
   await saveCart(products);
+
+  // products.push(item);
 };
 
 const showCart = async () =>{
