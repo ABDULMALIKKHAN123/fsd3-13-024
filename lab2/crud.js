dfa07012948afaf93fb9873b2183175bfa7a65fd
abdul malik khan <am6690707@gmail.com>
2026-09-15 14:11:38 +0530

    prg5 and lab2 complete

--- lab2/crud.js
+++ lab2/crud.js
@@ -83,101 +83,168 @@
 
 
 // sir ka code hai 👇
-import readline from "readline/promises";
-import { writeFile, readFile } from "fs/promises";
+// import readline from "readline/promises";
+// import { writeFile, readFile } from "fs/promises";
 
-import { stdin, stdout } from "process";
-import { log } from "console";
+// import { stdin, stdout } from "process";
+// import { log } from "console";
 
-const FILE = "products.json";
+// const FILE = "products.json";
 
-const saveCart = async (cart) => {
-  await writeFile(FILE, JSON.stringify(cart, null, 2));
-};
+// const saveCart = async (cart) => {
+//   await writeFile(FILE, JSON.stringify(cart, null, 2));
+// };
 
-const getCart = async () => {
-  const data = await readFile(FILE, "utf-8");
-  return JSON.parse(data);
-};
+// const getCart = async () => {
+//   const data = await readFile(FILE, "utf-8");
+//   return JSON.parse(data);
+// };
 
-const addToCart = async (item) => {
-  const products = await getCart();
-  const productFound = products.find ((p)=>p.id==item.id);
-  if (productFound){
-    productFound.qty += item.qty;
-    console.log('product in cart quantity updated');
-  }
-  else{
-    product.push(item);
-    console.log("Product added successfully 👍");
+// const addToCart = async (item) => {
+//   const products = await getCart();
+//   const productFound = products.find ((p)=>p.id==item.id);
+//   if (productFound){
+//     productFound.qty += item.qty;
+//     console.log('product in cart quantity updated');
+//   }
+//   else{
+//     product.push(item);
+//     console.log("Product added successfully 👍");
     
-  }
-  await saveCart(products);
+//   }
+//   await saveCart(products);
 
-  // products.push(item);
-};
+//   // products.push(item);
+// };
 
-const showCart = async () =>{
-     console.log("showCart");
-} 
+// const showCart = async () =>{
+//      console.log("showCart");
+// } 
 
-const updateCart = async () =>{
-      console.log("updateCart");
-}
+// const updateCart = async () =>{
+//       console.log("updateCart");
+// }
 
-const deleteFromCart = async () =>{
-      console.log("deleteFromCart");
-}
+// const deleteFromCart = async () =>{
+//       console.log("deleteFromCart");
+// }
+
+
+// const main = async () => {
+//   const cin = readline.createInterface({ input: stdin, output: stdout });
+//   let choice;
+//   do 
+//     console.log("Welcome to shopping cart 🛍️");
+//     console.log("1 ------- Add to cart");
+//     console.log("2 ------- Show Cart");
+//     console.log("3 ------- Remove Item");
+//     console.log("4 ------- Update Quantity");
+//     console.log("5 ------- Checkout");
+//     choice = await cin.question("Enter your choice:");
+//     switch (Number(choice)) {
+//       case 1:
+//         let data = await cin.question('Enter id,name,price,qty:');
+//         let p = data.split(",");
+        
+//         let q = p.map((item)=>item.trim());
+        
+//         let[id,name,price,qty]=q;
+//         console.log(id,name,price,qty);
+//         const product = {
+//             id:Number(id),
+//             name,
+//             price:Number(price),
+//             qty:Number(qty),
+//         };
+//         // console.log(product);
+//         await addToCart(product);
+        
+
+        
+//         break;
+//       case 2:
+//         showCart();
+//         break;
+//       case 3:
+//         deleteFromCart();
+//         break;
+//       case 4:
+//         updateCart();
+//         break;
+//       case 5:
+//         console.log("See you later...😃");
+//         process.exit();
+//         break;
+//       default:
+//         console.log("Invalid choice! try again 🛑");
+//     }
+//   } while (choice != "5");
+
+//   cin.close();
+// };
+
+// main();
+
 
 
 const main = async () => {
-  const cin = readline.createInterface({ input: stdin, output: stdout });
+  const cin = readline.createInterface({
+    input: stdin,
+    output: stdout
+  });
+
   let choice;
-  do 
+
+  do {
     console.log("Welcome to shopping cart 🛍️");
     console.log("1 ------- Add to cart");
     console.log("2 ------- Show Cart");
     console.log("3 ------- Remove Item");
     console.log("4 ------- Update Quantity");
     console.log("5 ------- Checkout");
-    choice = await cin.question("Enter your choice:");
-    switch (Number(choice)) {
-      case 1:
-        let data = await cin.question('Enter id,name,price,qty:');
-        let p = data.split(",");
-        
-        let q = p.map((item)=>item.trim());
-        
-        let[id,name,price,qty]=q;
-        console.log(id,name,price,qty);
-        const product = {
-            id:Number(id),
-            name,
-            price:Number(price),
-            qty:Number(qty),
-        };
-        // console.log(product);
-        await addToCart(product);
-        
 
-        
+    choice = await cin.question("Enter your choice: ");
+
+    switch (Number(choice)) {
+
+      case 1:
+        let data = await cin.question("Enter id,name,price,qty: ");
+
+        let p = data.split(",");
+        let q = p.map((item) => item.trim());
+
+        let [id, name, price, qty] = q;
+
+        const product = {
+          id: Number(id),
+          name,
+          price: Number(price),
+          qty: Number(qty)
+        };
+
+        await addToCart(product);
         break;
+
       case 2:
-        showCart();
+        await showCart();
         break;
+
       case 3:
-        deleteFromCart();
+        await deleteFromCart();
         break;
+
       case 4:
-        updateCart();
+        await updateCart();
         break;
+
       case 5:
         console.log("See you later...😃");
-        process.exit();
         break;
+
       default:
         console.log("Invalid choice! try again 🛑");
     }
+
   } while (choice != "5");
 
   cin.close();
--- lab3/prg5.js
+++ lab3/prg5.js
@@ -11,7 +11,25 @@ const server = http.createServer((req,res)=>{
     // API Tester:- postman, echo api,thunder client
     
 
+if(req.url==='/'){
+    res.setHeader("content-type","text/html");
+    res.end("<h1> Hello Server</h1>");
+}
+else if(req.url==="/products"){
+    const products =[
+        {id: 1, name: "Bottle"},
+        {id: 2, name: "Laptop"},
+    ];
+    res.writeHead(200,{"content-type":"applicaton/json"});
+    res.end(JSON.stringify(products));
 
+}
+else{
+    res.end();
+}
 
 
 });
+
+server.listen(5001, ()=> console.log("prg5 is runnig"));
+
