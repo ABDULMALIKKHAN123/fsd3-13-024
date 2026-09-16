3486f095f65ab664b0c57c7cf7278f826b21742c
abdul malik khan <am6690707@gmail.com>
2026-09-16 17:51:00 +0530

    file 5

--- lab2/file4.js
+++ lab2/file4.js
@@ -3,14 +3,14 @@ import { mkdir } from "fs/promises";
 
 
 // comment hai ye pahle se 👇
-await mkdir ('upload')
-console.log("folder created");
+// await mkdir ('upload')
+// console.log("folder created");
 
-await mkdir ("upload/resume");
-console.log("resume created under upload folder");
+// await mkdir ("upload/resume");
+// console.log("resume created under upload folder");
 
-await mkdir("images/profile/logos",{ recursive:true})
-console.log("all folders created");
+// await mkdir("images/profile/logos",{ recursive:true})
+// console.log("all folders created");
 // comment hai ye pahle se 👆
 
 
