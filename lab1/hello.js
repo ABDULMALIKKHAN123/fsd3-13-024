597055f6d47a28a63ca1b0425bc5b5a27cdbad5a
abdul malik khan <am6690707@gmail.com>
2026-09-16 14:20:56 +0530

    function and const

--- lab1/hello.js
+++ lab1/hello.js
@@ -1,9 +1,14 @@
+
 function sayHello(name){
     console.log(`Welcome to ${name}`);
 }
 
 
 const sayHi = (name) => {
+    console.log(`Welcome to ${name}`);
+}
+
+const sayHy = (name) => {
     console.log(`Welcome to ${name}`);
 }
 
@@ -14,4 +19,6 @@ function sum(a,b){
 
 sayHello('Abdul Malik Khan')
 sayHi("Abdul Majid Khan")
+sayHy('Rajiya, Rabiya, Rafiya, Sadiya')
+
 console.log(`sum of 3 and 8 is = ${sum(3,8)}`);
